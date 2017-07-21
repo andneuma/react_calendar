e4fcce764935cd8d584e02f37a93db6ae1dc9398
andneuma <andneuma@posteo.de>
2017-07-21 21:57:44 +0200

    Finish base setup for inserting appointments via ajax

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,21 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointment = Appointment.new
+    populate_appointments
+  end
+
+  def create
+    @appointment = Appointment.create(appt_params)
+    populate_appointments
+  end
+
+  private
+
+  def appt_params
+    params.require(:appointment).permit(:title, :appt_time)
+  end
+
+  def populate_appointments
+    @appointments = Appointment.all.sort_by(&:appt_time)
   end
 end
